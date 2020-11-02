23d3413c5648edc6a08ca973056bb366aabcad21
Jonathan Chan <jonathan.chan41@gmail.com>
2020-11-02 16:54:17 -0600

    fixed FKREase, added full texts, docstrings

--- README.md
+++ README.md
@@ -10,11 +10,25 @@ This project aims to make the IPCC summary more readable with the following obje
 - Reduce the length of the report via text summarization (Spacy NLP packages)
 - Reduce the reading time using standard word calculations (ex: assuming a reading pace of 250 words per minute, new reading time is ... )
 
+
 ## Results
 
 ![results](img/results.png)
 
 ![baseline comparison](img/baseline.png)
+
+
+### Example summary
+
+#### ORIGINAL
+
+Human activities are estimated to have caused approximately 1.0°C of global warming5 above pre-industrial levels, with a likely range of 0.8°C to 1.2°C. Global warming is likely to reach 1.5°C between 2030 and 2052 if it continues to increase at the current rate. (high confidence) (Figure SPM.1) Reflecting the long-term warming trend since pre-industrial times, observed global mean surface temperature (GMST) for the decade 2006–2015 was 0.87°C (likely between 0.75°C and 0.99°C)6 higher than the average over the 1850–1900 period (very high confidence). Estimated anthropogenic global warming matches the level of observed warming to within ±20% (likely range). Estimated anthropogenic global warming is currently increasing at 0.2°C (likely between 0.1°C and 0.3°C) per decade due to past and ongoing emissions (high confidence). Warming greater than the global annual average is being experienced in many land regions and seasons, including two to three times higher in the Arctic. Warming is generally higher over land than over the ocean. (high confidence)  Trends in intensity and frequency of some climate and weather extremes have been detected over time spans during which about 0.5°C of global warming occurred (medium confidence).
+
+#### LSA Summary
+Human activities are estimated to have caused approximately 1.0°C of global warming5 above pre-industrial levels, with a likely range of 0.8°C to 1.2°C. Reflecting the long-term warming trend since pre-industrial times, observed global mean surface temperature (GMST) for the decade 2006–2015 was 0.87°C (likely between 0.75°C and 0.99°C)6 higher than the average over the 1850–1900 period (very high confidence). Warming greater than the global annual average is being experienced in many land regions and seasons, including two to three times higher in the Arctic. Trends in intensity and frequency of some climate and weather extremes have been detected over time spans during which about 0.5°C of global warming occurred (medium confidence).
+
+#### LexRank Summary
+Human activities are estimated to have caused approximately 1.0°C of global warming5 above pre-industrial levels, with a likely range of 0.8°C to 1.2°C. Reflecting the long-term warming trend since pre-industrial times, observed global mean surface temperature (GMST) for the decade 2006–2015 was 0.87°C (likely between 0.75°C and 0.99°C)6 higher than the average over the 1850–1900 period (very high confidence). Warming greater than the global annual average is being experienced in many land regions and seasons, including two to three times higher in the Arctic. Trends in intensity and frequency of some climate and weather extremes have been detected over time spans during which about 0.5°C of global warming occurred (medium confidence). 
 
 ## Limitations
 
@@ -30,7 +44,5 @@ This project aims to make the IPCC summary more readable with the following obje
 4. Finalized summarization of text, insight generation (percent reduction of each)
 
 ## TO DO:
-- export full text (raw, summaries x2)
-- Clean code 
-- Docstrings
 - Containerize / add requirements.txt
+- Discussion of results
--- script.py
+++ script.py
@@ -17,7 +17,12 @@ output_folder = "data/"
 
 #SCRAPING FUNCTIONS
 def get_soup(url): 
-    """returns the BeautifulSoup object from a given url"""
+    """returns the BeautifulSoup object from a given url
+    
+    Input:
+        url: url string from IPCC website
+        
+    """
     html = requests.get(url).text
 
     soup = BeautifulSoup(html, 'html.parser')
@@ -25,8 +30,12 @@ def get_soup(url):
     return soup
 
 def get_raw_dict(soup, pattern):
-    """returns the paragraphs from the IPCC BeautifulSoup using the pattern regex pattern
-    Also returns the full text as a text string"""
+    """returns a dict of paragraphs, text content from the URL
+    BeautifulSoup using the 'pattern' as regex pattern
+    
+    Input:
+        soup: BeautifulSoup object (output from get_soup())
+        pattern: regex pattern string"""
     
     prog = re.compile(pattern)
     ind = 0
@@ -62,8 +71,15 @@ def get_raw_dict(soup, pattern):
 #READING FORMULAS FROM: https://medium.com/analytics-vidhya/visualising-text-complexity-with-readability-formulas-c86474efc730
 #ARI: https://readabilityformulas.com/automated-readability-index.php
 #Flesch Reading age, ease: https://en.wikipedia.org/wiki/Flesch%E2%80%93Kincaid_readability_tests
+#baseline reading score for average americans:  https://centerforplainlanguage.org/what-is-readability/#:~:text=Your%20audience's%20reading%20age%20is%20lower%20than%20you%20think&text=U.S.%20illiteracy%20statistics%20from%20the,12%20to%2014%20years%20old).
+
 def num_syllables(word, word_dict):
-    """return num of syllables in word using either CMU dict or number of vowels"""
+    """return num of syllables in word (int) using either CMU dict or number of vowels
+    
+    input:
+        word: individual word string
+        word_dict: dict from nltk.corpus (gives number of syllables)
+    """
     
     syl_count = 0
     vowels = {"a","e","i","o","u","y"}
@@ -86,42 +102,21 @@ def num_syllables(word, word_dict):
         syl_count = vowel_count
     return syl_count
 
-def flesch_reading_ease(text):
-    """return reading ease for a full text string
-    
-    90-100 understood by 11 year old student
-    60-10 easily understood by 13-15 year old student
-    0-30 best understood by university graduates"""
-    
-    all_sents = text.split(". ")
-    
-    for sent in all_sents:
-        sent_list = sent.split()
-        for word in sent_list:
-            if not word.isalpha():
-                sent_list.remove(word)
-        syl_count = 0
-        total_sent = 1
-        total_words = 0
-        
-        if len(sent_list) == 0:
-            return None
-        else:
-            for word in sent_list:
-                total_words += 1
-                syl_count += num_syllables(word, p_dict)
-                
-        reading_score = 206.835 - 1.015*(total_words/total_sent) - 84.6*(syl_count/total_words)
-        
-        return reading_score   
-
 def SMOG(text):
-    """return the smog grade """
-    all_sents = text.split(". ")
+    """return the SMOG reading level (float) for a full document string
     
+    Assume sentences are separated by '. '
+    
+    input:
+        text: string of document 
+    
+    
+    """
+    all_sents = text.split(". ")
     num_polysyl = 0 
     num_words = 0
     num_sents = len(all_sents)
+    
     for sent in all_sents:
         sent_list = sent.split()
         for word in sent_list:
@@ -133,12 +128,15 @@ def SMOG(text):
     return smog_grade
 
 def flesch_kincaid_reading_age(text):
+    """return Flesch Kincaid Reading Level (float) for a full document string
+
+    Assume sentences are separated by '. '
     
-    """reading age 
-    https://medium.com/analytics-vidhya/visualising-text-complexity-with-readability-formulas-c86474efc730"""
+    input:
+        text: string of document 
+    """
     
     all_sents = text.split(". ")
-    
     total_words = 0
     total_sents = len(all_sents)
     total_syllables = 0
@@ -150,41 +148,86 @@ def flesch_kincaid_reading_age(text):
             total_syllables += num_syllables(word, p_dict)
             
     fkra = 0.39*(total_words/total_sents) + 11.8*(total_syllables/total_words) - 15.59
+    return fkra 
+
+def flesch_reading_ease(text):
+    """return Flesch Kincaid Reading Ease score (float) for a full document string
+
+    Assume sentences are separated by '. '
     
-    return fkra if fkra > 0 else 0
+    input:
+        text: string of document 
+    """
+    all_sents = text.split(". ")
+    syl_count = 0
+    total_sent = len(all_sents)
+    total_words = 0
+    
+    for sent in all_sents:
+        word_list = sent.split()
+        for word in word_list:
+            if not word.isalpha():
+                word_list.remove(word)
+    if total_sent == 0:
+        return None
+    else:
+        for sent in all_sents:
+            for word in sent:
+                total_words += 1
+                syl_count += num_syllables(word, p_dict)
+                
+    reading_score = 206.835 - 1.015*(total_words/total_sent) - 84.6*(syl_count/total_words)
+    return reading_score
 
 def ARI(text):
-    """returns the Automated readability index for a given text string"""
-    
+    """return ARI reading level (float) for a full document string
+    Assume sentences are separated by '. '
+    input:
+        text: string of document 
+    """
     all_sents = text.split(". ")
-    
     num_sents = len(all_sents)
     num_char = 0
     num_words = 0
-    
+
     for sent in all_sents:
         sent_list = sent.split()
         for word in sent_list:
             num_char += len(word)
             num_words += 1
     ari_score = 4.71*(num_char/num_words) + 0.5*(num_words/num_sents) - 21.43
-
     return ari_score
 
 def word_count(text):
-    """return the word count for a given text string"""
+    """return the word count (int) for a given text string
     
+    Assume sentences are separated by '. '
+    
+    input:
+        text: string of document 
+    
+    """
     all_words = text.split()
     return len(all_words)
 
 def reading_time(text):
-    """Return the number of minutes for reading a given text (assuming 250 word/min)"""
+    """Return the number of minutes (float) for reading a given text (assuming 250 word/min)
+    
+    Assume sentences are separated by '. '
+    
+    input:
+        text: string of document 
+    """
     return word_count(text) / 250
 
 #TEXT SUMMARIZE FUNCTIONS
 
 def lsa_summarize(input_dict):
-    """Return the summarized text given the raw_dict from get_raw_dict()"""
+    """Return the summarized text string given the raw_dict from get_raw_dict()
+    
+    input: 
+        input_dict: dictionary containing document content under "RAW_TEXT" key (first input from get_raw_dict())
+    """
     
     summarizer_lsa = LsaSummarizer()
     summarizer_lsa = LsaSummarizer(Stemmer("english"))
@@ -199,7 +242,11 @@ def lsa_summarize(input_dict):
     return lsa_fulltext
     
 def lexrank_summarize(input_dict):
-    """Return the summarized text given the raw_dict from get_raw_dict()"""
+    """Return the summarized text string given the raw_dict from get_raw_dict()
+    
+    input: 
+    input_dict: dictionary containing document content under "RAW_TEXT" key (first input from get_raw_dict())
+    """
     summarizer_lr = LexRankSummarizer()
     lr_fulltext = ""
     
@@ -212,11 +259,12 @@ def lexrank_summarize(input_dict):
 
 #RESULTS FUNCTIONS
 def get_results_df(raw_str, lsa_str, lr_str):
-    """return a dataframe of all reading metric values for given input text strings
+    """return a dataframe (pandas dataframe) of all reading metric values for given input text strings
     
     raw_str: original text 
     lsa_str: text summarized using LsaSummarizer
     lr_str: text summarized using LexRankSummarizer
+    
     """
     columns = ["word count", "reading time (min)", "ARI", "SMOG", "Flesch Kincaid Reading Ease", "Flesch Kincaid Reading Age"]
     fn_list = [word_count, reading_time, ARI, SMOG, flesch_reading_ease, flesch_kincaid_reading_age]
@@ -224,7 +272,6 @@ def get_results_df(raw_str, lsa_str, lr_str):
         "Raw": {},
         "LSA": {},
         "LexRank": {}
-
     }
 
     for i, fn in enumerate(fn_list):
@@ -235,25 +282,21 @@ def get_results_df(raw_str, lsa_str, lr_str):
 
     results_df = pd.DataFrame.from_dict(results_dict, orient = "index",
                                        columns=columns).round(2)
-
     return results_df
 
 def get_baseline_df(results):
-    """return a baseline comparison dataframe given a results dataframe
+    """return a baseline comparison dataframe (pandas dataframe) given a results dataframe
     
     Assume baseline reading level is approximately 7th grade (12-14 year old):
-    https://centerforplainlanguage.org/what-is-readability/#:~:text=Your%20audience's%20reading%20age%20is%20lower%20than%20you%20think&text=U.S.%20illiteracy%20statistics%20from%20the,12%20to%2014%20years%20old).
     
     """
     
     baseline_vals = [7.0, 7.0, 80.00, 7.0]
     baseline_df = results.iloc[1:, 2:]
     baseline_df.loc["baseline"] = baseline_vals
-    
     return baseline_df
 
-#MAIN
-def main():
+def main(return_dfs=False):
     soup = get_soup(ipcc_url)
     pattern = "^( ?[A-Z]\.[1-9\.]+) *([^{}]*)(.*)"
 
@@ -261,16 +304,31 @@ def main():
     lsa_text = lsa_summarize(raw_dict)
     lr_text = lexrank_summarize(raw_dict)
 
-
+    #get results and put them output into csv files
     results_df = get_results_df(raw_text, lsa_text, lr_text)
     baseline_df = get_baseline_df(results_df)
-
+    
     results_df.to_csv(path_or_buf="results/results.csv")
     print("FILE CREATED: ", "results/results.csv")
     baseline_df.to_csv(path_or_buf="results/baseline_comparison.csv")
     print("FILE CREATED: ", "results/baseline_comparison.csv")
     
+    #put strings into text files
+    texts = [raw_text, lsa_text, lr_text]
+    names = ["original_text.txt", "lsa_summary.txt", "lexrank_summary.txt"]
+    
+    for i, text in enumerate(texts):
+        name = names[i]
+        filename = "results/" + name
+        new_text = text.replace(". ", ". \n\n")
+        with open(filename, "w") as f:
+            f.write(new_text)
+        print("FILE CREATED: ", filename)
+    
+    if return_dfs:
+        return results_df, baseline_df
     print("OUTPUTS WRITTEN INTO FOLDER: results")
+
 
 if __name__ == "__main__":
     p_dict = cmudict.dict()
